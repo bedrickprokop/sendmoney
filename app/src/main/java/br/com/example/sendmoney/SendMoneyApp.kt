f4f1a2ce49ab101cd2b0ba9eba789611374398ca
bedrick <bedrick.prokop@protonmail.com>
2020-02-29 12:42:48 -0300

    SendMoney project initial structure

---
+++ app/src/main/java/br/com/example/sendmoney/SendMoneyApp.kt
@@ -0,0 +1,10 @@
+package br.com.example.sendmoney
+
+import android.app.Application
+
+class SendMoneyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/br/com/example/sendmoney/SendMoneyConsts.kt
@@ -0,0 +1,4 @@
+package br.com.example.sendmoney
+
+interface SendMoneyConsts {
+}
